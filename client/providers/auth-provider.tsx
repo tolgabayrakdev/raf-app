1c8784867d4ba7f1cd5f9932468e497385a31c49
Tolga BAYRAK <bayraktolga28@gmail.com>
2024-10-26 20:48:46 +0300

    auth provider added.

---
+++ client/providers/auth-provider.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { ReactNode, useEffect, useState } from "react";
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+function AuthProvider({ children }: AuthProviderProps) {
+    const router = useRouter()
+    const [loading, setLoading] = useState(true);
+    const [accessDenied, setAccessDenied] = useState(false);
+
+    useEffect(() => {
+        const verifyAuthToken = async () => {
+            try {
+                const res = await fetch("http://localhost:1234/api/auth/verify", {
+                    method: 'POST',
+                    credentials: 'include',
+                });
+                if (res.status === 200) {
+                    setLoading(false);
+                } else if (res.status === 401 || res.status === 403) {
+                    setLoading(false);
+                    setAccessDenied(true);
+                } else {
+                    setLoading(false);
+                    setAccessDenied(true);
+                }
+            } catch (error) {
+                setLoading(false);
+                setAccessDenied(true);
+            }
+        };
+        verifyAuthToken();
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="flex h-screen items-center justify-center">
+                <div className="relative">
+                    <div className="animate-spin rounded-full h-6 w-6 border-2 border-t-transparent border-black"></div>
+                </div>
+            </div>
+        )
+    } else if (accessDenied) {
+        setLoading(true);
+        setTimeout(() => {
+            router.push('/sign-in');
+        }, 1000);
+    }
+
+    return <>{children}</>;
+}
+
+export default AuthProvider;
